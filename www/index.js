dd7cff512ca2e33f938af8382ffab2e9710aeba2
Tim Banon <izmbanon@gmail.com>
2020-03-11 19:33:50 -0400

    Updated framework

--- www/index.js
+++ www/index.js
@@ -1,6 +1,6 @@
 function init() {
-    var host = window.location.host.split('.');
+    var host = window.location.host.split('.'); console.log({host})
     if(host.length>2) { 
-        fetch('https://banon.cf/'+host[0]).then(e => console.log(e));
+        fetch('https://banon.cf/'+host[0]).then(e => console.log('html',e));
     }
 }
