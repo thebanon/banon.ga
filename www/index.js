f1469cf4383f10c6e7bfaa3c6fdded407bb55961
Tim Banon <izmbanon@gmail.com>
2020-03-11 18:32:15 -0400

    Update index.js

--- www/index.js
+++ www/index.js
@@ -1,4 +1,4 @@
 function init() {
     var host = window.location.host.split('.');
-    if(host.length>2) { window.location.href = 'https://banon.cf/'+host[0]; }
+    //if(host.length>2) { window.location.href = 'https://banon.cf/'+host[0]; }
 }
