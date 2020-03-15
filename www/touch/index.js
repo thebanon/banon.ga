eeacd9bb60e1a20f7ba8a8e5f6452f4c6c33d213
Tim Banon <izmbanon@gmail.com>
2020-03-15 15:23:38 -0400

    Updated /jumplate & /touch projects
    Fixed JumpLate chat input CSS
    Debugged Touch Library

--- www/touch/index.js
+++ www/touch/index.js
@@ -5,44 +5,6 @@ if('serviceWorker' in navigator) {
   });
 }
 window.touch = {
-    local: {
-      dbl: null, 
-      drag: {
-        start: { x:0, y:0 }, 
-        offset: {}
-      },
-      press: null, 
-      threshold: { drag: 25 },
-      type: null
-    },
-    handler: (event,type=event.type) => {
-        if(type === 'touchstart') {          
-            touch.local.drag.start.x = event.touches[0].pageX, touch.local.drag.start.y = event.touches[0].pageY;
-            if(touch.local.dbl) { clearTimeout(touch.local.dbl); touch.local.dbl = null;  touch.local.type = 'dbltap'; touch.events(event.target,touch.local.type); } 
-            else { touch.local.type = 'press'; touch.local.dbl = setTimeout(() => { touch.local.dbl = null;  touch.events(event.target,touch.local.type); },300); }
-        }
-        else if (type === "touchmove") { clearTimeout(touch.local.dbl);
-          touch.local.drag.offset = {},
-          touch.local.drag.offset.x = Math.abs(touch.local.drag.start.x - event.touches[0].pageX),
-          touch.local.drag.offset.y = Math.abs(touch.local.drag.start.y - event.touches[0].pageY);   
-          touch.local.dbl = null; 
-          touch.local.type = 'drag'; 
-          if(
-            (touch.local.drag.offset.x > touch.local.threshold.drag) || 
-            (touch.local.drag.offset.y > touch.local.threshold.drag)) { 
-            touch.events(event.target,'drag');
-          } 
-          else { touch.local.type = null; }
-        }
-        else if (type === "touchend") { 
-          clearTimeout(touch.local.press); touch.local.press = null; touch.local.type = null; 
-          touch.local.type === 'dbltap' ? touch.local.type === null : null;
-          setTimeout(() => { document.body.removeAttribute('error'); }, 3000);
-        }
-    },
-    events: (target,t,evt=t?t:'tap') => { document.body.dataset.touch = evt; }
-}
-window.touch = {
     drag: {
         start: { x:0, y:0 }, 
         offset: {},        
@@ -54,51 +16,67 @@ window.touch = {
     timer: null,
     now: 0,
     then: 0,
-    handler: (event,type=event.type) => { //console.log({type,evt:touch.local.type});
+    handler: (event,type=event.type) => { //console.log({type});
         if (type === "touchstart") {
             touch.now = new Date().getTime(), touch.then = touch.now - touch.ghost;
             if((touch.then < 300) && (touch.then > 0)) { 
                 touch.event = 'dbl';
-                document.body.dataset.touch = touch.event;
                 clearTimeout(touch.timer); touch.timer = null; 
+                touch.events(event.target,touch.event);
                 console.log(touch.event);
             } 
             touch.press = setTimeout(() => { 
                 clearTimeout(touch.press); touch.press = null; 
                 clearTimeout(touch.timer); touch.timer = null; 
                 touch.event = "hold"; console.log(touch.event);
-                document.body.dataset.touch = touch.event;
+                touch.events(event.target,touch.event);
             }, 500);
         }
-        else if (1<0 && type === "touchmove") { 
+        else if (type === "touchmove") {
+          clearTimeout(touch.press); touch.press = null;
           clearTimeout(touch.timer); touch.timer = null;
           touch.drag.offset = {},
           touch.drag.offset.x = Math.abs(touch.drag.start.x - event.touches[0].pageX),
           touch.drag.offset.y = Math.abs(touch.drag.start.y - event.touches[0].pageY);
-          touch.event = 'drag'; 
+          touch.event = touch.event==='dragstart' ? 'dragmove' : 'dragstart'; console.log('dragstart');
           if(
             (touch.drag.offset.x > touch.drag.threshold) || 
             (touch.drag.offset.y > touch.drag.threshold)) { 
-            touch.events(event.target,'drag');
+            touch.events(event.target,touch.event);
           } 
           else { touch.event = null; }
         }
         else if (type == "touchend") {
-            if((touch.then < 300) && (touch.then > 0)) {  } 
-            else {
-                if(touch.event==="hold") {
+
+
+                if(["dragstart","dragmove"].includes(touch.event)) {
+
                     clearTimeout(touch.press); touch.press = null;
-                    clearTimeout(touch.timer); touch.timer = null;
-                    touch.event = null;
+                    clearTimeout(touch.timer); touch.timer = null;                    
+                    touch.event = 'dragend'; console.log(touch.event);
+                                    
                 } else {
-                    touch.timer = setTimeout(() => { 
-                        touch.event = 'tap';                 
-                        clearTimeout(touch.press); touch.press = null;       
-                        clearTimeout(touch.timer); touch.timer = null; 
-                        document.body.dataset.touch = touch.event;
-                    }, 300);
+        
+                    if(touch.then===0 || touch.then > 300) { 
+                            if(touch.event==="hold") {
+                                clearTimeout(touch.press); touch.press = null;
+                                clearTimeout(touch.timer); touch.timer = null;
+                                touch.events(event.target,touch.event);
+                                touch.event = null;
+                            } else {
+                                touch.timer = setTimeout(() => { 
+                                    touch.event = 'tap';                 
+                                    clearTimeout(touch.press); touch.press = null;       
+                                    clearTimeout(touch.timer); touch.timer = null; 
+                                    touch.events(event.target,touch.event);
+                                    console.log(touch.event);
+                                }, 300);
+                            }
+
+                    }
+
                 }
-            }
+
             touch.ghost = touch.now;
             clearTimeout(touch.press); touch.press = null;
         }
