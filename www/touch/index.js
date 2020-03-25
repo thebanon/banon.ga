539f4587d49cc951f0121210615c2c3f29e3f76f
Tim Banon <izmbanon@gmail.com>
2020-03-25 18:05:36 -0400

    Updated framework

--- www/touch/index.js
+++ www/touch/index.js
@@ -1,8 +1,44 @@
-window.touch = {
+window.is = {
+  cam: view => { return ['cam'].includes(view) ? true : false; },
+  dark: (pct,date=new Date()) => { document.body.dataset.mode = pct<0.5 ? "lite" : "dark"; },
+  local: () => { return window.location.origin.includes('localhost') ? true : false; },
+  external: u => { return u.includes('://') || u.includes(';base64,'); },
+  mobile: () => { 
+    var ua = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    ua ? document.body.dataset.mobi=true : document.body.dataset.mobi ? document.body.removeAttribute('data-mobi') : null; 
+    return ua;
+  },
+  numeric: a => { return a===0 || parseInt(a)>0 ? true : false; },
+  online: () => { return firebase.auth().currentUser; },
+  overflow: (node,dir) => { 
+    if(node == null) { return null; }
+    return node['scroll'+dir] > node['client'+dir] ? node : is.overflow(node.parentNode,dir);
+  },
+  sunset: (sunrise, sunset, now) => { document.body.dataset.mode = now < sunrise || now > sunset ? 'dark' : 'lite'; }
+};
+function init() {
+    var host = window.location.host.split('.'); console.log({host})
+    if(host.length>2) { 
+        fetch('https://banon.cf/'+host[0]).then(e => console.log('html',e));
+    }
+}
+
+window.onTouch = {
     drag: {
+        angle: 0,
         start: { x:0, y:0 }, 
         offset: { x:0, y:0 },      
-        threshold: 86
+        percent: { x:0, y:0 },
+        distance: { x:0, y:0 },
+        wasd: '',
+        scroll: 0,
+        swipe: null,
+        events: ['dragstart', 'dragmove', 'dragend'],
+    },
+    thresh: { 
+        dblTap: () => { return is.mobile() ? 300 : 300  }, 
+        drag: 0, 
+        hold: () => { return is.mobile() ? 500 : 500  } 
     },
     event: '',
     ghost: 0,
@@ -11,69 +47,81 @@ window.touch = {
     now: 0,
     then: 0,
     touches: null,
+    blur: target => { event.target.type === 'text' ? null : document.activeElement.blur() },
     handler: (event,type=event.type) => { //console.log({type});
-        if (type === "touchstart") { console.clear();
-            touch.touches = event.touches; 
-            touch.now = new Date().getTime(), touch.then = touch.now - touch.ghost;
-            touch.drag.offset.x = this.offsetLeft, touch.drag.offset.y = this.offsetTop;
-            touch.drag.start.x = touch.touches[0].pageX, touch.drag.start.y = touch.touches[0].pageY;
-            if((touch.then < 300) && (touch.then > 0)) { 
-                touch.event = 'dbl';
-                clearTimeout(touch.timer); touch.timer = null; 
-                touch.events(event.target,touch.event);
+        if (type === "touchstart") { //console.clear();
+            onTouch.touches = event.touches; 
+            onTouch.now = new Date().getTime(), onTouch.then = onTouch.now - onTouch.ghost;
+            onTouch.drag.start.x = onTouch.touches[0].pageX, onTouch.drag.start.y = onTouch.touches[0].pageY;
+            if((onTouch.then < onTouch.thresh.dblTap()) && (onTouch.then > 0)) { 
+                onTouch.event = 'dbl';
+                clearTimeout(onTouch.timer); onTouch.timer = null; 
+                onTouch.events(event,onTouch.event,type);
             }
-            touch.press = setTimeout(() => {
-                touch.event= ["dragstart", "dragmove"].includes(touch.event) ? 'dragmove' : 'dragstart';
-                clearTimeout(touch.press); touch.press = null; 
-                clearTimeout(touch.timer); touch.timer = null; 
-                touch.event = "hold"; touch.events(event.target,touch.event);
-            }, 500);
+            onTouch.press = setTimeout(() => {
+                onTouch.event= ["dragstart", "dragmove"].includes(onTouch.event) ? 'dragmove' : 'dragstart';
+                clearTimeout(onTouch.press); onTouch.press = null; 
+                clearTimeout(onTouch.timer); onTouch.timer = null; 
+                onTouch.event = "hold"; onTouch.events(event,onTouch.event,type);
+            }, onTouch.thresh.hold());
         }
         else if (type === "touchmove") {
-            var moveX = event.touches[0].pageX, moveY = event.touches[0].pageY;
-            var origX = touch.drag.start.x, origY = touch.drag.start.y;
-            var dragMove = Math.abs(touch.drag.start - moveX);
-            var a = Math.abs(touch.drag.start.x - moveX), b = Math.abs(touch.drag.start.y - moveY), c = Math.sqrt( a*a + b*b );            
-            var dragging = c > touch.drag.threshold;
-            if(dragging) {
-                console.log({c,dragging})
-                touch.event= ["dragstart", "dragmove"].includes(touch.event) ? 'dragmove' : 'dragstart';
-                clearTimeout(touch.press); touch.press = null;
-                clearTimeout(touch.timer); touch.timer = null; 
-                touch.events(event.target,touch.event);
-            }
+
+            if(onTouch.event === "dragstart") { is.overflow(event.target,'Width') ? onTouch.drag.scroll = is.overflow(event.target,'Width').scrollLeft : null; }
+            //else {            
+                onTouch.drag.offset.x = event.touches[0].pageX, onTouch.drag.offset.y = event.touches[0].pageY;
+                var origX = onTouch.drag.start.x, origY = onTouch.drag.start.y;
+                onTouch.drag.distance.x = Math.abs(onTouch.drag.start.x - onTouch.drag.offset.x);
+                onTouch.drag.distance.y = Math.abs(onTouch.drag.start.y - onTouch.drag.offset.y);
+                var a = Math.abs(onTouch.drag.start.x - onTouch.drag.offset.x), b = Math.abs(onTouch.drag.start.y - onTouch.drag.offset.y), c = Math.sqrt( a*a + b*b );            
+                var dragging = c > onTouch.thresh.drag;
+                //if(dragging) {
+                    //console.log(onTouch.drag.offset);
+                    onTouch.event = ["dragstart", "dragmove"].includes(onTouch.event) ? 'dragmove' : 'dragstart';
+                    onTouch.drag.percent.x = 100*(onTouch.drag.offset.x-onTouch.drag.start.x)/document.body.clientWidth;
+                    onTouch.drag.percent.y = 100*(onTouch.drag.offset.y-onTouch.drag.start.y)/document.body.clientHeight
+                    var angle = onTouch.drag.angle = Math.abs(Math.atan2(onTouch.drag.offset.y,onTouch.drag.offset.x) * 180 / Math.PI);
+                    onTouch.drag.swipe = (angle>0 && angle<45) || (angle>135 && angle<180);
+                    if(Math.sign(onTouch.drag.percent.x) === -1) { onTouch.drag.wasd = '-1'; }
+                    else if(Math.sign(onTouch.drag.percent.x) === 1) { onTouch.drag.wasd = '1'; }
+                    clearTimeout(onTouch.press); onTouch.press = null;
+                    clearTimeout(onTouch.timer); onTouch.timer = null; 
+                    onTouch.events(event,onTouch.event,type);
+                //}
+            //}
+
         }
         else if (type == "touchend") {
+                
+                if(["dragstart","dragmove"].includes(onTouch.event)) {
 
-                if(["dragstart","dragmove"].includes(touch.event)) {
-
-                    clearTimeout(touch.press); touch.press = null;
-                    clearTimeout(touch.timer); touch.timer = null;                    
-                    touch.event = 'dragend'; touch.events(event.target,touch.event);
+                    clearTimeout(onTouch.press); onTouch.press = null;
+                    clearTimeout(onTouch.timer); onTouch.timer = null;                    
+                    onTouch.event = 'dragend'; onTouch.events(event,onTouch.event,type);
                                     
                 } else {
         
-                    if(touch.then===0 || touch.then > 300) { 
-                            if(touch.event==="hold") {
-                                clearTimeout(touch.press); touch.press = null;
-                                clearTimeout(touch.timer); touch.timer = null;
-                                //touch.events(event.target,touch.event);
-                                touch.event = null;
+                    if(onTouch.then===0 || onTouch.then > onTouch.thresh.dblTap()) { 
+                            if(onTouch.event==="hold") {
+                                clearTimeout(onTouch.press); onTouch.press = null;
+                                clearTimeout(onTouch.timer); onTouch.timer = null;
+                                //onTouch.events(event,onTouch.event);
+                                onTouch.event = null;
                             } else {
-                                touch.timer = setTimeout(() => { 
-                                    touch.event = 'tap';                 
-                                    clearTimeout(touch.press); touch.press = null;       
-                                    clearTimeout(touch.timer); touch.timer = null; 
-                                    touch.events(event.target,touch.event);
-                                }, 300);
+                                onTouch.timer = setTimeout(() => { 
+                                    onTouch.event = 'tap';                 
+                                    clearTimeout(onTouch.press); onTouch.press = null;       
+                                    clearTimeout(onTouch.timer); onTouch.timer = null; 
+                                    onTouch.events(event,onTouch.event,type);
+                                }, onTouch.thresh.dblTap());
                             }
 
                     }
 
                 }
 
-            touch.ghost = touch.now;
-            clearTimeout(touch.press); touch.press = null;
+            onTouch.ghost = onTouch.now;
+            clearTimeout(onTouch.press); onTouch.press = null;
         }
     },
     events: (target,t,type=t?t:'tap') => { 
